1e97313e21bfc0eb06b30e35530927adca5b903e
unknown <mandalbis1729@gmail.com>
2020-09-23 18:36:02 +0530

    dev2

--- statefulwidget/lib/main.dart
+++ statefulwidget/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           ),
           body: Center(
             child: Container(
-              child: Text('$_counter'),
+              child: Text('this is the second test' + '$_counter'),
             ),
           ),
           floatingActionButton: FloatingActionButton(
