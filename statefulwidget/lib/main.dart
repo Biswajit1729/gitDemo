5a2c5f2d8dd56e0c3f29db7b64144288d00d8a59
unknown <mandalbis1729@gmail.com>
2020-09-23 18:32:49 +0530

    initial commit

---
+++ statefulwidget/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  var _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return StatefulBuilder(
+      builder: (context, setState) => MaterialApp(
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: Center(
+            child: Container(
+              child: Text('$_counter'),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => setState(() => _counter++),
+            child: Text("click"),
+          ),
+        ),
+      ),
+    );
+  }
+}
